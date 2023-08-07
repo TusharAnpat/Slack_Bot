619c0b32eb5be6bda948e9ffff80938b18958348
TusharAnpat <tusharanpat7777@gmail.com>
2023-08-07 12:03:25 +0530

    slack bot changes added

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const SLACK_OAUTH_TOKEN = 'xoxb-5697181536114-5699643433988-KhYNRFl7r5bwov28s8W15gvh'
+export const SLACK_OAUTH_TOKEN = 'SLACK_OAUTH_TOKEN'
 // this is the channel you want your bot online 
 export const BOT_CHANNEL = '#chatbot' 
